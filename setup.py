e46724e99e6d4616f47baee72114f045063445d8
Subhash Bhushan <subhash.bhushan@gmail.com>
2019-04-29 18:21:31 -0700

    Bump version: 0.0.9 → 0.0.11

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def read(*names, **kwargs):
 
 setup(
     name='authentic',
-    version='0.0.9',
+    version='0.0.11',
     license='BSD 3-Clause License',
     description='Comprehensive Authentication Package',
     long_description='%s\n%s' % (
--- src/authentic/__init__.py
+++ src/authentic/__init__.py
@@ -2,7 +2,7 @@ from protean.conf import active_config
 
 from . import config
 
-__version__ = '0.0.9'
+__version__ = '0.0.11'
 
 
 # Update the config here so that loading the repo will load the config
